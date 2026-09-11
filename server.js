730569fdf446943dc735a53afedda088a9152ae0
Ajeet <121504490+Ajeet9918@users.noreply.github.com>
2026-09-11 16:46:24 +0530

    Make port dynamic for deployment

--- server.js
+++ server.js
@@ -144,4 +144,5 @@ setInterval(() => {
     }
 }, 1000 / 20);
 
-server.listen(3000, () => console.log('Server running on :3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on :${PORT}`));
